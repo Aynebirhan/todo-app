b7c1a9f9f01da49e4fc2737e37fa8f5037236618
Aynebirhan <aynebirhanamsalu@gmail.com>
2024-01-03 13:07:43 -0800

    first database connection

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = 8000;
 
 //init app
 const app = express();
+
+const connectionUrl = "mongodb://localhost:27017/todoDb";
+mongoose.connect(connectionUrl)
+  .then(() => 
+    console.log("Database connection successfully")
+  )
+  .catch((error) => 
+    console.log(error.message)
+  )
 
 //set view engine
 app.set("view engine", "ejs");
