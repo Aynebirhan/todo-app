d8584f1a52bb5985ca8c8fea18914e7e770875e8
Aynebirhan <aynebirhanamsalu@gmail.com>
2024-08-08 17:33:35 -0700

    create pages and routes

--- init/mongodb.js
+++ init/mongodb.js
@@ -1,15 +1,13 @@
 const mongoose = require("mongoose");
 
-
-const connectMonogodb = async() =>{
-    try{
-        await mongoose.connect(process.env.CONNECTION_URL);
-        console.log("Database connected successfully")
-
-    }catch(error){
-        console.log(error.message);
-        process.exit(1);
-    }
-} 
+const connectMonogodb = async () => {
+  try {
+    await mongoose.connect(process.env.CONNECTION_URL);
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
+};
 
 module.exports = connectMonogodb;
